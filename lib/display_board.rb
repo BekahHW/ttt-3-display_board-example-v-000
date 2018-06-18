d2805e1041a5fb0dea3ca9349a3ddf40ff05f0f2
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-06-18 17:46:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-puts display_board =  " " | " " | " "
-                     ----------------
+puts display_board =  " "|" "|" "
+                      -----------
